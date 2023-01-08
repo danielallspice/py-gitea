f7852dc317d74dfff2efda0e0a46cb73172d7121
Daniel Lindmark <daniel@allspice.io>
2023-01-08 15:44:27 -0600

    Add sha parameter to get_commit()

--- gitea/apiobject.py
+++ gitea/apiobject.py
@@ -392,11 +392,13 @@ class Repository(ApiObject):
         """Get all Issues of this Repository (open and closed)"""
         return self.get_issues_state(Issue.OPENED) + self.get_issues_state(Issue.CLOSED)
 
-    def get_commits(self) -> List["Commit"]:
+    def get_commits(self, sha='main') -> List["Commit"]:
         """Get all the Commits of this Repository."""
         try:
+            params = {"sha":sha}
             results = self.gitea.requests_get_paginated(
-                Repository.REPO_COMMITS % (self.owner.username, self.name)
+                Repository.REPO_COMMITS % (self.owner.username, self.name),
+                params
             )
         except ConflictException as err:
             logging.warning(err)
